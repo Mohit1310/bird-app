0b80a9f91884e59840d022988cfa2a53e8204948
Mohit Dayma <70746260+Mohit1310@users.noreply.github.com>
2023-04-10 06:10:46 +0530

    starting project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css'
+
+function App() {
+  return (
+    // BEM
+    <div className='app'>
+      <h1>Hi everyone</h1>
+
+      {/* Sidebar */}
+
+      {/* Feed */}
+
+      {/* Widgets */}
+    </div>
+  )
+}
+
+export default App
